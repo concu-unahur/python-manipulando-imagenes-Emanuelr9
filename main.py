0b38d925c5b32f46a5116f8afcb16ed52e1852c1
Emanuelr9 <emanuelr9@yahoo.com>
2020-02-20 21:47:31 -0300

    tratando de concatenar

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from api import PixabayAPI
 import sys
 import threading
+from archivos import leer_imagen, escribir_imagen
+from concatenacion import concatenar_vertical
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
@@ -17,5 +19,14 @@ urls = api.buscar_imagenes(query, cantidad)
 
 for u in urls:
     logging.info(f'Descargando {u}')
-    #api.descargar_imagen(u)
-    threading.Thread(target=(lambda: api.descargar_imagen(u))).start()
+    api.descargar_imagen(u)
+    #threading.Thread(target=(lambda: api.descargar_imagen(u))).start()
+
+
+logging.info(f'Lista{api.lista_imagenes}')
+
+lista=api.lista_imagenes
+
+for i in range(5):
+    escribir_imagen('concatenada-vertical.jpg',concatenar_vertical([lista[i], lista[i+1]]))    
+    #escribir_imagen('concatenada-horizontal.jpg', concatenar_horizontal([lista_imagenes[3], lista_imagenes[4]]))  
